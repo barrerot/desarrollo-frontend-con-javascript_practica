503148b12c91deb078f8ab99bbfc2e7d11f0218f
Carlos Barrero Martínez <barrerot@gmail.com>
2023-10-15 17:12:31 +0200

    minor change

--- tweet-list/tweetListModel.js
+++ tweet-list/tweetListModel.js
@@ -1,15 +1,15 @@
 const transformTweets = (tweets) => {
   return tweets.map(tweet => ({
-    handler: tweet.author,
+    handler: tweet.name,
     date: new Date().toISOString(),
-    message: tweet.message,
-    likes: tweet.likes.length,
-    photo: tweet.image,
+    message: tweet.description,
+    likes: tweet.price,
+    photo: tweet.picture,
   }))
 }
 
 export const getTweets = async () => {
-  const url = "http://localhost:8000/api/tweets";
+  const url = "http://localhost:8000/api/items";
   let parsedTweets = [];
 
   try {
